47d12b3e3bf7709fad13f8ce608020ab557ed167
IBUKUN-BIT <alukoibk@icloud.com>
2025-10-16 15:04:26 +0100

    Update GalleryPage.tsx-iwanger
    Update GalleryPage.tsx

--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -3,14 +3,14 @@
 import { BaseLayout } from "../Components/layout/BaseLayout"
 import { GalleryHero } from "../Components/Gallery/GalleryHero"
 import { InteractiveGallery } from "../Components/Gallery/InteractiveGallery"
-import { GalleryStats } from "../Components/Gallery/GalleryStats"
+// import { GalleryStats } from "../Components/Gallery/GalleryStats"
 
 export function GalleryPage() {
   return (
     <BaseLayout>
       <GalleryHero />
       <InteractiveGallery />
-      <GalleryStats />
+      {/* <GalleryStats /> */}
     </BaseLayout>
   )
 }
